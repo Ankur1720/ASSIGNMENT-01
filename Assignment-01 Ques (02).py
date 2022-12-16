b6c7e6380372c2fc532e3fcfa4401e62461fe7fa
Ankur1720 <120731049+Ankur1720@users.noreply.github.com>
2022-12-16 23:03:42 +0530

    Add files via upload

---
+++ Assignment-01 Ques (02).py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Ques -(02) Write a Python program that accepts a word from the user and reverse it ?
+
+
+# In[8]:
+
+
+word = input("Input a word to reverse = ")
+for i in range(len(word) - 1, -1, -1):
+  print(word[i], end="")
+print()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
---
+++ Assignment-01.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Ques -01) Write a Python program to get the Fibonacci series between 0 to 50 ?
+
+
+# In[2]:
+
+
+no1 = 0
+no2 = 1
+while no2<50:
+    print(no2,end = " ")
+    no1,no2 = no2,no1+no2
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
